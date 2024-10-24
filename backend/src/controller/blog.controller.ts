203b254c48c6f74fe1c8422a5e4363ad47da22d6
jkmerndev <jkmerndev@gmail.com>
2024-10-24 20:08:44 +0530

    created a route to update post

--- backend/src/controller/blog.controller.ts
+++ backend/src/controller/blog.controller.ts
@@ -2,16 +2,16 @@ import { Context } from "hono";
 import { prismaConnection } from "../../utils/prismaClient";
 
 export const getBlog = async (c: Context) => {
-  const prisma = prismaConnection(c)
+  const prisma = prismaConnection(c);
   try {
-    const id = c.req.param('id') 
+    const id = c.req.param("id");
     const result = await prisma.post.findUnique({
-      where :{
-        id : id
-      }
-    })
+      where: {
+        id: id,
+      },
+    });
 
-    return c.json({status : true ,  post : result})
+    return c.json({ status: true, post: result });
   } catch (error) {}
   return c.json({ success: false, message: "Internal server error" });
 };
@@ -20,9 +20,9 @@ export const getAllBlogs = async (c: Context) => {
   const prisma = prismaConnection(c);
   try {
     const result = await prisma.post.findMany({
-      take:5  
-    })
-    return c.json({status :true, posts : result})
+      take: 5,
+    });
+    return c.json({ status: true, posts: result });
   } catch (error) {
     return c.json({ status: false, msg: "internal Server error" }, 500);
   }
@@ -59,6 +59,33 @@ export const createNewBlog = async (c: Context) => {
 };
 
 export const updateBlog = async (c: Context) => {
-  c.text("createNewBlog");
-  console.log(c.req.param("id"));
+  const id = c.req.param("id");
+  const userId = c.get("userId");
+  const prisma = prismaConnection(c);
+  try {
+    const body = await c.req.json();
+    const user = await prisma.user.findUnique({
+      where: {
+        id: userId,
+      },
+    });
+
+    if (!user) {
+      return c.json({ status: false, msg: "Please Login to update Details" });
+    }
+
+    const update = await prisma.post.update({
+      where : {
+        id : id
+      }, 
+      data : {
+        title : body.title || undefined,
+        content : body.content || undefined,
+        published : body.published || undefined
+      }
+    })
+    return c.json({status : true, msg:"Post updated Successfully"})
+  } catch (error) {
+    return c.json({ status: false, msg: "Please Login to update Details" });
+  }
 };
