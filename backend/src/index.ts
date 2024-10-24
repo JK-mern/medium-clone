0824b5ca10f9cd5ac4d54c3422ad4c8cb5f3697f
jkmerndev <jkmerndev@gmail.com>
2024-10-24 14:56:54 +0530

    created schema and did migration also setup environment variable

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,15 +1,18 @@
-import { Hono } from 'hono'
-import authenticationRoute from './routes/authentication.route'
-import blogRoute from './routes/blog.route'
-const app = new Hono()
+import { Hono } from "hono";
+import authenticationRoute from "./routes/authentication.route";
+import blogRoute from "./routes/blog.route";
+const app = new Hono<{
+  Bindings: {
+    DATABASE_URL: string;
+    SECRET_KEY: string;
+  };
+}>();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.get("/", (c) => {
+  return c.text("Hello Hono!");
+});
 
+app.route("/api/v1/auth", authenticationRoute);
+app.route("/api/v1/blog", blogRoute);
 
-app.route('/api/v1/auth' ,authenticationRoute)
-app.route('/api/v1/blog' ,blogRoute)
-
-
-export default app
+export default app;
