3d5163e79cfde01fda82496328abd094fe6c7094
jkmerndev <jkmerndev@gmail.com>
2024-10-24 19:35:58 +0530

    completed controller function to fetch blog details

--- backend/src/controller/blog.controller.ts
+++ backend/src/controller/blog.controller.ts
@@ -2,15 +2,26 @@ import { Context } from "hono";
 import { prismaConnection } from "../../utils/prismaClient";
 
 export const getBlog = async (c: Context) => {
+  const prisma = prismaConnection(c)
   try {
+    const id = c.req.param('id') 
+    const result = await prisma.post.findUnique({
+      where :{
+        id : id
+      }
+    })
+
+    return c.json({status : true ,  post : result})
   } catch (error) {}
-  return c.json({ success: true, message: "Blog is this " });
+  return c.json({ success: false, message: "Internal server error" });
 };
 
 export const getAllBlogs = async (c: Context) => {
   const prisma = prismaConnection(c);
   try {
-    const result = await prisma.post.findMany()
+    const result = await prisma.post.findMany({
+      take:5  
+    })
     return c.json({status :true, posts : result})
   } catch (error) {
     return c.json({ status: false, msg: "internal Server error" }, 500);
