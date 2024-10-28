0eaece3892e0942cc510154663b8164c857a613b
jkmerndev <jkmerndev@gmail.com>
2024-10-28 21:13:11 +0530

    completed blogs component

--- frontend/src/Pages/Blogs.tsx
+++ frontend/src/Pages/Blogs.tsx
@@ -1,13 +1,16 @@
 import BlogCard from "@/components/BlogCard";
+import Loader from "@/components/Loader";
 import Navbar from "@/components/Navbar";
 import useFetchAllBlogs from "@/hooks/useFetchAllBlogs";
 import { Blogposts } from "@/types/types";
+import { useState } from "react";
 
 function Blogs() {
   const blogposts : Blogposts[]  = useFetchAllBlogs();
   return (
     <div>
       <Navbar />
+      {blogposts.length ===0 && <Loader size="large" />}
       {blogposts.length > 0 &&
         blogposts.map((blogpost) => (
           <BlogCard   key={blogpost.id}  blogpost={blogpost}/>
