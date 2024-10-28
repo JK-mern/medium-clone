7a969b36f7465d0e6d6b95204a37a5fd41466e89
jkmerndev <jkmerndev@gmail.com>
2024-10-28 22:19:32 +0530

    created a blog component to display a particular blog

--- frontend/src/Pages/Blog.tsx
+++ frontend/src/Pages/Blog.tsx
@@ -1,13 +1,29 @@
-
+import Loader from "@/components/Loader";
+import Navbar from "@/components/Navbar";
+import { useFetchBlogDetails } from "@/hooks/useFetchBlogDetails";
 
 function Blog() {
+  const blog = useFetchBlogDetails();
   return (
     <div>
-        <main className='grid grid-cols-2 min-h-screen'>
-         
-        </main>
+      <main className="">
+        <Navbar />
+        {blog ? (
+          <div className="flex flex-col max-w-screen-xl  mx-auto space-y-1  mt-8">
+            <h1 className="  font-bold leading-tight text-3xl  ">
+              {blog?.title}
+            </h1>
+            <p className="text-muted-foreground text-xs font-inter">
+              Posted on {blog?.publishedDate}
+            </p>
+            <p className="pt-7">{blog?.content}</p>
+          </div>
+        ) : (
+          <Loader />
+        )}
+      </main>
     </div>
-  )
+  );
 }
 
-export default Blog
+export default Blog;
